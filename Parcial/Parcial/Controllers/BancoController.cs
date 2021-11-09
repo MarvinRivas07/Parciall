a05b72e0a2f6381faf2cbf82de18401cc3606727
MarvinRivas07 <88003076+MarvinRivas07@users.noreply.github.com>
2021-11-09 11:26:26 -0600

    Parcial

---
+++ Parcial/Parcial/Controllers/BancoController.cs
@@ -0,0 +1,49 @@
+﻿using Parcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial.Controllers
+{
+    public class BancoController : Controller
+    {
+        // GET: Banco
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Cajero()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Correcto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cajero (Cajero caj)
+        {
+            if (caj.Monto%5==0)
+            {
+                return Redirect("Correcto");
+            }
+            else
+            {
+                return Redirect("Error");
+            }
+        }
+    }
+}
